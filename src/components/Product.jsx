acbaf9d766b8c1c5a54ace3d8a6c4fb4b70a8393
Aniket Patel <aniket.patel9786@gmail.com>
2023-04-07 01:11:49 +0530

    update1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,21 +4,23 @@ import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
-      <div className='h-[310px] border-2 border-black/25 p-2 m-5'>
+      <div className='h-[290px] border-2 border-black/25 p-2 m-5'>
             <Link to={`/product/${product._id}`}>
             
             <img src={product.image} alt={product.name} className='h-40 w-52' />
 
-            <div className='w-[150px] text-[12px] break-words overflow-hidden p-2 pl-3'>         
-                <strong>{product.name}</strong>
-            </div>
+            <div className='pt-2 pl-2'>
+                <div className='w-[200px] h-[39px] text-[12px] break-words overflow-hidden'>         
+                    <strong>{product.name}</strong>
+                </div>
 
-            <div className='py-0 px-5 text-sm font-medium'>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </div>
+                <div className='pt-1 text-sm font-medium'>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </div>
 
-            <div className='py-1 px-5 text-lg font-semibold'>
-                ${product.price}
+                <div className='py-1 text-lg font-semibold'>
+                    ${product.price}
+                </div>
             </div>
 
         </Link>
